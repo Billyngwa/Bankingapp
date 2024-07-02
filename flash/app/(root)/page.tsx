1ed88d888df297555deee465e0c19818da955df9
Billyngwa <billytatangwa5@gmail.com>
2024-07-02 18:10:54 +0100

    file and folder setup complete

--- flash/app/(root)/page.tsx
+++ flash/app/(root)/page.tsx
@@ -1,8 +1,19 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <HeaderBox
+        type="greeting"
+        title="Welcome"
+        user="Billy"
+        subtext="Access and manage your accounts and transactions"
+
+        />
+        <TotalBalanceBox/>
+    </div>
   )
 }
 
